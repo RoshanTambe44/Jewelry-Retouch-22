57744003937cdadac3b330995b7f6994bbe39706
Ay7983 <anjaliyadavyadav571@gmail.com>
2025-01-27 12:52:38 +0530

    files

--- src/components/Last.jsx
+++ src/components/Last.jsx
@@ -4,29 +4,31 @@ import "../styles/Last.css";
 const Last = () => {
   return (
     <footer className="last-footer">
-      <div className="footer-text">
-        <p>
-          &copy; 2025 SNB Web Services Pvt. Ltd. | CIN: U72900UP2017PTC097495.All rights reserved.
-        </p>
-      </div>
-      <div className="footer-links">
-        <a href="https://snbwebservices.com/privacy-policy/" target="_blank" rel="noopener noreferrer">
-          Privacy Policy
-        </a>
-        <a href="https://snbwebservices.com/ccpa-privacy-policy/" target="_blank" rel="noopener noreferrer">
-          CCPA Privacy Policy
-        </a>
-        <a href="https://snbwebservices.com/gdpr-privacy-policy/" target="_blank" rel="noopener noreferrer">
-          GDPR Privacy Policy
-        </a>
-      </div>
-      <div className="footer-credits">
-        <p>
-          Designed and Developed by{" "}
-          <a href="https://www.houseofmarktech.com/home" target="_blank" rel="noopener noreferrer">
-            House Of Marktech
+      <div className="footer-containers">
+        <div className="footer-text">
+          <p>
+            &copy; 2025 SNB Web Services Pvt. Ltd. | CIN: U72900UP2017PTC097495. All rights reserved.
+          </p>
+        </div>
+        <div className="footer-links">
+          <a href="https://snbwebservices.com/privacy-policy/" target="_blank" rel="noopener noreferrer">
+            Privacy Policy
           </a>
-        </p>
+          <a href="https://snbwebservices.com/ccpa-privacy-policy/" target="_blank" rel="noopener noreferrer">
+            CCPA Privacy Policy
+          </a>
+          <a href="https://snbwebservices.com/gdpr-privacy-policy/" target="_blank" rel="noopener noreferrer">
+            GDPR Privacy Policy
+          </a>
+        </div>
+        <div className="footer-credits">
+          <p>
+            Designed and Developed by{" "}
+            <a href="https://www.houseofmarktech.com/home" target="_blank" rel="noopener noreferrer">
+              House Of Marktech
+            </a>
+          </p>
+        </div>
       </div>
     </footer>
   );
